83ffeaa8ce346971ff1fd59b1810f2300ae7c57e
Guilherme Rossa <rossaguilherme05@gmail.com>
2025-08-15 21:59:11 -0300

    alterado logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
         }`}>
           <div className="flex items-center space-x-2">
             <img 
-              src="/LOGO BARBER ZAP 1.PNG" 
+              src="/LOGO BARBER ZAP 1.PNG" with="80px" 
               alt="BarberZap Logo" 
               className={`rounded-lg object-cover transition-all duration-300 ${
                 isScrolled ? 'h-8 w-8' : 'h-10 w-10'
